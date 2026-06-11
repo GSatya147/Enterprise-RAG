d968b59d5cc5a15765546e37af5f07d5f19ed67b
GSatya147 <gvvsn147@gmail.com>
2026-06-11 23:07:13 +0100

    code: add chunker skeleton

---
+++ src/chunker.py
@@ -0,0 +1,53 @@
+import os
+
+from loader import Loader
+
+class Chunker:
+    def __init__(self, chunk_size, loader_docs, model):
+        self.CHUNK_SIZE = chunk_size
+        self.CHUNK_OVERLAP = int(0.15 * self.CHUNK_SIZE)
+        self.model = model
+
+
+
+        try:
+            self.tokenizer = self.model.tokenizer
+
+            self.tokenized_data = self.tokenizer.encode(data)
+        except Exception as e:
+            print(e)
+
+    def corpus_chunker(self)-> list[dict]:
+        self.chunks = []
+
+        i=0
+        while i < len(self.tokenized_data):
+            self.chunks.append(self.tokenized_data[i: self.CHUNK_SIZE + i])
+            i+=self.CHUNK_SIZE - self.CHUNK_OVERLAP
+
+        document_dict = dict()
+        chunk_dict_list = []
+
+        for i in range(len(self.chunks)):
+            document_dict = {
+                "content": self.tokenizer.decode(self.chunks[i]),
+                "metadata": {
+                    "source": "manual",
+                    "document_name": os.path.basename(self.FILE_PATH),
+                    "chunk_index": i,
+                    "token_count": len(self.chunks[i])
+                }
+            }
+            chunk_dict_list.append(document_dict)
+
+        return chunk_dict_list
+
+    def sanity_check(self)-> None:
+        print(f"Total tokens: {len(self.tokenized_data)}")
+        print(f"Chunks: {len(self.chunks)}")
+        print(f"First chunk length: {len(self.chunks[0])}")
+        print(f"Pen-ultimate chunk length: {len(self.chunks[-2])}")
+        print(f"Ultimate chunk length: {len(self.chunks[-1])}") 
+
+if __name__=="__main__":
+    pass  
--- src/loader.py
+++ src/loader.py
@@ -1,13 +1,22 @@
 from langchain_community.document_loaders import DirectoryLoader
 from langchain_community.document_loaders import PyMuPDFLoader 
 
-loader = DirectoryLoader(
-    path="papers/",
-    glob="*.pdf",
-    loader_cls=PyMuPDFLoader
-)
+class Loader:
+    def __init__(self, directory):
+        self.dir = directory
 
-docs = loader.load()
+    def corpus_loader(self):
+        """ Uses langchain's PymuPDFLoadercomponent and give the pdf into [{"metadata": , "page_content": },{}...each page-section has one dict] """
+        
+        loader = DirectoryLoader(
+            path=self.dir,
+            glob="*.pdf",
+            loader_cls=PyMuPDFLoader
+        )
 
-print(docs[0].metadata)
-print(docs[100].metadata)
+        self.docs = loader.load()
+
+        return self.docs
+
+# print(docs[0].metadata)
+# print(docs[100].metadata)
