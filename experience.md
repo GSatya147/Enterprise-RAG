006567903547a9daf500d2edbe7904ee675760f2
GSatya147 <gvvsn147@gmail.com>
2026-06-12 14:14:42 +0100

    code: add embedder pinecone vector store, voyage cloud calls

--- experience.md
+++ experience.md
@@ -1,3 +1,7 @@
 #### failure experience:
 
 1. **Loading document corpus:** Tried langchain components explicitly for loading document corpus of pdfs which has 2 column styled, 1 column styles arxiv pdf, `PDFPlumber` loader with custom configurations performed awful. tried others `PymuPDF` performed significantly better with minimal/default configurations, this shows pymupdf has more layout level awareness even tho it doesn't respect the spaces and layout decors, it gets the job done with the text.
+
+2. **Data cleaning:** Sometimes we need to overengineer, tbh whatever we do in dataset engineering is never over engineering at all. Ignoring references/bibliographies in the data, automatically pollutes the individual chunks, even tho reranker ranks them low and prevents them never contaminating LLM context, still indexing-wasted-resources are not worth for individual chunks which are not useful. especially at scale. SPEND SOME TIME IN DATASET ENGINEERING.
+
+3. **Embedding:**
--- src/embedder.py
+++ src/embedder.py
@@ -1,33 +1,36 @@
 import os
 
-import chromadb
 from chunker import Chunker
 from dotenv import load_dotenv
 from loader import Loader
 from pinecone import ServerlessSpec
 from pinecone.grpc import PineconeGRPC as Pinecone
 from sentence_transformers import SentenceTransformer
+import voyageai
 
 load_dotenv()
-key=os.getenv("PINECONE_API_KEY")
-if not key:
-    raise EnvironmentError("Environment variable error")
+pinecone_key=os.getenv("PINECONE_API_KEY")
+voyage_key=os.getenv("VOYAGE_API_KEY")
 
 class VectorEmbedder:
-    def __init__(self, chunk_docs, model):
+    def __init__(self, chunk_docs):
         self.EMBED_AND_STORE = True
         self.chunker_result = chunk_docs
-        self.model = model
+
+        try:
+            self.vo_client = voyageai.Client(api_key=voyage_key)
+        except Exception as e:
+            print(e)
 
     def corpus_embedder(self):
         try:
             self.embeddings = []
             running_tokens = 0
             current_batch = []
-            for chunk in self.chunker_result:
+            for i, chunk in enumerate(self.chunker_result):
                 if running_tokens + chunk["metadata"]["token_count"] > 5000: 
-                    self.embeddings.extend(self.model.encode(current_batch))
-
+                    self.embeddings.extend(self.vo_client.embed(texts=current_batch, model=os.getenv("EMBEDDING_MODEL")).embeddings)
+                    print(f"Batch: {i+1}, done")
                     running_tokens = 0
                     current_batch = []
                 
@@ -35,13 +38,13 @@ class VectorEmbedder:
                 current_batch.append(chunk["content"])
             
             if current_batch:
-                self.embeddings.extend(self.model.encode(current_batch))
+                self.embeddings.extend(self.vo_client.embed(texts=current_batch, model=os.getenv("EMBEDDING_MODEL")).embeddings)
 
+            return self.embeddings
+        
         except Exception as e:
             print(e)
         
-        return self.embeddings
-    
     def vector_store(self):
         if self.EMBED_AND_STORE:
             self.corpus_embedder()
@@ -51,7 +54,7 @@ class VectorEmbedder:
             for i,chunk in enumerate(self.chunker_result):
                 field = {
                     "id": f"doc_{i}",
-                    "values": self.embeddings[i].tolist(),
+                    "values": self.embeddings[i],
                     "metadata":{
                         "source": chunk["metadata"].get("source"),
                         "content": chunk.get("content")
@@ -59,26 +62,26 @@ class VectorEmbedder:
                 }
                 data_dict_field.append(field)
             
-        try:
-            pc = Pinecone(api_key=key)
-            index_name = "satya-RAGLens"
+            try:
+                pc = Pinecone(api_key=pinecone_key)
+                index_name = "satya-RAGLens"
 
-            if not pc.has_index(index_name):
-                pc.create_index(
-                    name=index_name,
-                    vector_type="dense",
-                    dimension=1024,
-                    metric="cosine",
-                    spec=ServerlessSpec(cloud="aws", region="us-east-1")
-                )
+                if not pc.has_index(index_name):
+                    pc.create_index(
+                        name=index_name,
+                        vector_type="dense",
+                        dimension=1024,
+                        metric="cosine",
+                        spec=ServerlessSpec(cloud="aws", region="us-east-1")
+                    )
 
-            index = pc.Index(index_name)
+                index = pc.Index(index_name)
 
-            if self.EMBED_AND_STORE:
-                index.upsert(vectors=data_dict_field, namespace="RAGLens-references")
-        
-        except Exception as e:
-            print(e)
+                if self.EMBED_AND_STORE:
+                    index.upsert(vectors=data_dict_field, namespace="RAGLens-references")
+            
+            except Exception as e:
+                print(e)
         
 if __name__=="__main__":
     loader_obj = Loader("papers/") 
@@ -93,7 +96,7 @@ if __name__=="__main__":
     chunker_obj = Chunker(1000, docs, tokenizer)
     chunks = chunker_obj.corpus_chunker()
 
-    embedder_obj = VectorEmbedder(chunks, model=model)
+    embedder_obj = VectorEmbedder(chunks)
     embeddings = embedder_obj.vector_store()
 
     if embedder_obj.EMBED_AND_STORE:
