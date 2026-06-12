17f2d625d5fd174372cbb0f5f89be7d876def3d0
GSatya147 <gvvsn147@gmail.com>
2026-06-12 13:17:06 +0100

    code: add embedder vector store, store lever

--- src/embedder.py
+++ src/embedder.py
@@ -1,7 +1,17 @@
+import os
+
 import chromadb
 from chunker import Chunker
+from dotenv import load_dotenv
 from loader import Loader
+from pinecone import ServerlessSpec
+from pinecone.grpc import PineconeGRPC as Pinecone
 from sentence_transformers import SentenceTransformer
+
+load_dotenv()
+key=os.getenv("PINECONE_API_KEY")
+if not key:
+    raise EnvironmentError("Environment variable error")
 
 class VectorEmbedder:
     def __init__(self, chunk_docs, model):
@@ -32,6 +42,44 @@ class VectorEmbedder:
         
         return self.embeddings
     
+    def vector_store(self):
+        if self.EMBED_AND_STORE:
+            self.corpus_embedder()
+
+            data_dict_field: list[dict] = []
+
+            for i,chunk in enumerate(self.chunker_result):
+                field = {
+                    "id": f"doc_{i}",
+                    "values": self.embeddings[i].tolist(),
+                    "metadata":{
+                        "source": chunk["metadata"].get("source"),
+                        "content": chunk.get("content")
+                    }
+                }
+                data_dict_field.append(field)
+            
+        try:
+            pc = Pinecone(api_key=key)
+            index_name = "satya-RAGLens"
+
+            if not pc.has_index(index_name):
+                pc.create_index(
+                    name=index_name,
+                    vector_type="dense",
+                    dimension=1024,
+                    metric="cosine",
+                    spec=ServerlessSpec(cloud="aws", region="us-east-1")
+                )
+
+            index = pc.Index(index_name)
+
+            if self.EMBED_AND_STORE:
+                index.upsert(vectors=data_dict_field, namespace="RAGLens-references")
+        
+        except Exception as e:
+            print(e)
+        
 if __name__=="__main__":
     loader_obj = Loader("papers/") 
     docs = loader_obj.corpus_loader()
@@ -46,6 +94,8 @@ if __name__=="__main__":
     chunks = chunker_obj.corpus_chunker()
 
     embedder_obj = VectorEmbedder(chunks, model=model)
+    embeddings = embedder_obj.vector_store()
+
     if embedder_obj.EMBED_AND_STORE:
-        embeddings = embedder_obj.corpus_embedder()
-        print(embeddings)
+        print(embedder_obj.embeddings)
+        print(len(embedder_obj.embeddings))
