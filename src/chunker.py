395be450d48b98f692af3febf219f67a2217d3e6
GSatya147 <gvvsn147@gmail.com>
2026-06-12 11:50:04 +0100

    code: add chunker optimised structure

--- src/chunker.py
+++ src/chunker.py
@@ -1,5 +1,6 @@
 import os
 
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from loader import Loader
 
 class Chunker:
@@ -8,34 +9,35 @@ class Chunker:
         self.CHUNK_OVERLAP = int(0.15 * self.CHUNK_SIZE)
         self.model = model
 
-
+        self.data = loader_docs
 
         try:
             self.tokenizer = self.model.tokenizer
-
-            self.tokenized_data = self.tokenizer.encode(data)
         except Exception as e:
             print(e)
 
     def corpus_chunker(self)-> list[dict]:
-        self.chunks = []
 
-        i=0
-        while i < len(self.tokenized_data):
-            self.chunks.append(self.tokenized_data[i: self.CHUNK_SIZE + i])
-            i+=self.CHUNK_SIZE - self.CHUNK_OVERLAP
+        splitter = RecursiveCharacterTextSplitter(
+            chunk_size=self.CHUNK_SIZE,
+            chunk_overlap=self.CHUNK_OVERLAP,
+            add_start_index=True,
+            length_function=lambda text: len(self.tokenizer.encode(text))
+        )
+
+        self.chunks = splitter.split_documents(self.data)
 
         document_dict = dict()
         chunk_dict_list = []
 
         for i in range(len(self.chunks)):
             document_dict = {
-                "content": self.tokenizer.decode(self.chunks[i]),
+                "content": self.chunks[i].page_content,
                 "metadata": {
-                    "source": "manual",
-                    "document_name": os.path.basename(self.FILE_PATH),
+                    "source": self.chunks[i].metadata.get("source"),
+                    "document_name": os.path.basename(self.chunks[i].metadata.get("source")),
                     "chunk_index": i,
-                    "token_count": len(self.chunks[i])
+                    "token_count": len(self.tokenizer.encode(self.chunks[i].page_content))
                 }
             }
             chunk_dict_list.append(document_dict)
@@ -43,11 +45,16 @@ class Chunker:
         return chunk_dict_list
 
     def sanity_check(self)-> None:
-        print(f"Total tokens: {len(self.tokenized_data)}")
         print(f"Chunks: {len(self.chunks)}")
-        print(f"First chunk length: {len(self.chunks[0])}")
-        print(f"Pen-ultimate chunk length: {len(self.chunks[-2])}")
-        print(f"Ultimate chunk length: {len(self.chunks[-1])}") 
+        print(f"First chunk length: {len(self.tokeniser.encode(self.chunks[0].page_content))}")
+        print(f"Pen-ultimate chunk length: {len(self.tokeniser.encode(self.chunks[-2].page_content))}")
+        print(f"Ultimate chunk length: {len(self.tokeniser.encode(self.chunks[-1].page_content))}") 
 
 if __name__=="__main__":
-    pass  
+    loader_obj = Loader("papers/") 
+    docs = loader_obj.corpus_loader()
+
+    chunker_obj = Chunker(1000, docs)
+    chunks = chunker_obj.corpus_chunker()
+
+    chunker_obj.sanity_check()
