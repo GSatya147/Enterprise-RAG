5f543d6b0164f48aba79f87e0d5c76e6d41e7a73
GSatya147 <gvvsn147@gmail.com>
2026-06-17 21:53:54 +0100

    code: add ragas dataset generator skeleton

---
+++ eval/dataset_genertor.py
@@ -0,0 +1,15 @@
+import asyncio
+import json
+import os
+
+from src.chunker import Chunker
+from src.loader import DirectoryLoader
+
+class QAGenerator:
+    def __init__(self, chunks, n_sample=3):
+        pass
+
+    async def yu(self):
+        pass
+
+    
